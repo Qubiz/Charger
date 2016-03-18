be81ba94446884fb937ec01aafd5f9d1d81c5b6d
Qubiz <qubizwow@gmail.com>
2016-03-18 14:09:52 +0100

    ...

--- app/src/main/java/project/o/charger/MainActivity.java
+++ app/src/main/java/project/o/charger/MainActivity.java
@@ -1,13 +1,28 @@
 package project.o.charger;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import project.o.charger.adapters.PlannersListAdapter;
+import project.o.charger.planners.Planner;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Planner> planners;
+
+    ListView plannersListView;
+    PlannersListAdapter plannersListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +31,53 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        planners = new ArrayList<>();
+
+        plannersListAdapter = new PlannersListAdapter(this, planners);
+        plannersListView = (ListView) findViewById(R.id.planners_list_view);
+        if (plannersListView != null) {
+            plannersListView.setAdapter(plannersListAdapter);
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addNewPlanner();
+                }
+            });
+        }
+    }
+
+    private void addNewPlanner() {
+        final View newPlannerDialogView = getLayoutInflater().inflate(R.layout.dialog_fragment_new_planner, null);
+        final EditText plannerName = (EditText) newPlannerDialogView.findViewById(R.id.dialog_fragment_new_planner_edit_text_name);
+        final EditText plannerCreator = (EditText) newPlannerDialogView.findViewById(R.id.dialog_fragment_new_planner_edit_text_creator);
+
+        AlertDialog newPlannerDialog = new AlertDialog.Builder(this)
+                .setView(newPlannerDialogView)
+                .setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Planner newPlanner;
+                        if(plannerName.getText().toString().isEmpty() && plannerCreator.getText().toString().isEmpty()) {
+                            newPlanner = new Planner("Planner" + (planners.size() + 1), "Anoniem");
+                        } else if(plannerName.getText().toString().isEmpty()) {
+                            newPlanner = new Planner("Planner" + (planners.size() + 1), plannerCreator.getText().toString());
+                        } else if(plannerCreator.getText().toString().isEmpty()){
+                            newPlanner = new Planner(plannerName.getText().toString(), "Anoniem");
+                        } else {
+                            newPlanner = new Planner(plannerName.getText().toString(), plannerCreator.getText().toString());
+                        }
+                        planners.add(newPlanner);
+                        plannersListAdapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton("CANCEL", null)
+                .create();
+
+        newPlannerDialog.show();
     }
 
 }
--- app/src/main/java/project/o/charger/adapters/PlannersListAdapter.java
+++ app/src/main/java/project/o/charger/adapters/PlannersListAdapter.java
@@ -1,48 +1,57 @@
 package project.o.charger.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
-import project.o.charger.database.PlannersSQLiteHelper;
+import project.o.charger.R;
 import project.o.charger.planners.Planner;
 
-/**
- * Created by Mathijs on 17-3-2016.
- */
 public class PlannersListAdapter extends BaseAdapter {
 
     private Context context;
     private List<Planner> planners;
-    private PlannersSQLiteHelper database;
-
 
     public PlannersListAdapter(Context context, List<Planner> planners) {
         this.context = context;
         this.planners = planners;
-        this.database = new PlannersSQLiteHelper(context);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return planners.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return planners.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.planners_list_item, parent, false);
+
+        TextView name = (TextView) view.findViewById(R.id.planner_list_row_name);
+        TextView creator = (TextView) view.findViewById(R.id.planner_list_row_creator);
+
+        final Planner planner = planners.get(position);
+
+        name.setText(planner.getName());
+        creator.setText("created by " + planner.getCreator());
+
+        return view;
     }
 }
